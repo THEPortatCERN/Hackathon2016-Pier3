aa082238cfec4dfed123560adab894f52b2cac7e
Gonzalo Beviglia <gonzalo.beviglia@restorando.com>
2016-10-16 12:18:25 +0200

    Add + in phone

--- app/shared/userService.js
+++ app/shared/userService.js
@@ -10,14 +10,14 @@
         username: 'gonchub',
         name: 'Gonzalo Beviglia',
         email: 'gonzalo.beviglia@gmail.com',
-        phone: '541158930932',
+        phone: '+541158930932',
         location: '48.392738,14.062500'
       },
       2: {
         username: 'HelpingHand',
         name: 'Good Samaritan',
         email: 'helping.hand@gmail.com',
-        phone: '541187898765',
+        phone: '+541187898765',
         location: '-34.567382,-58.458784'
       }
     };
